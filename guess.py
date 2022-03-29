c2cec5fb666f4f71272d3bcccaaa5dd2f32cf41a
tinghsuanchen <ellie910401@gmail.com>
2022-03-30 02:14:18 +0800

    basic version

--- guess.py
+++ guess.py
@@ -1,9 +1,8 @@
-#產生一個隨機整數1~100
-#讓使用者重複輸入數字去猜
-#猜對了，印出"你答對了"
-#猜錯了，要告訴他比答案大還小
+
 import random
-r = random.randint(1,100)
+start = int(input('請使用者輸入開始值:'))
+end = int(input('請使用者輸入結束值:'))
+r = random.randint(start,end)
 count = 0
 while True:
 	count = count + 1
@@ -17,5 +16,5 @@ while True:
 	elif num < r:
 		print('比答案小')
 	print('這是你猜的第幾次',count,'次')
-	
+
 
